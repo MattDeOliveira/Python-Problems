92382ad2e4e37c098a0afeb3acbf170fce59e377
MattDeOliveira <mattoli829@gmail.com>
2021-05-10 18:21:36 -0400

    This is problem 4 for of the python problems, ("Largest Palindrome within range")

---
+++ problem_4.py
@@ -0,0 +1,17 @@
+field = range(100, 1000)
+big_drome = None
+
+for i in field:
+    for j in field:
+        ans = i * j
+        if str(ans) == str(ans)[::-1]: # all items in string to be reversed and compared
+            if ans > big_drome:
+                big_drome = ans
+
+
+print("""
+TASK: A palindromic number reads the same both ways. The largest palindrome made from the
+product of two 2-digit numbers is 9009 = 91 * 99.
+Find the largest palindrome made from the product of two 3-digit numbers.\n\n\
+""")
+print(big_drome)
