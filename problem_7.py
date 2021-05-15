0a8d3858c7bf5102e70e8e5abcca778bdf68cdc0
MattDeOliveira <mattoli829@gmail.com>
2021-05-15 15:44:01 -0400

    problem 7 completed

---
+++ problem_7.py
@@ -0,0 +1,13 @@
+primes_to_find = 100
+x = 2
+list_of_primes =[]
+while (len(list_of_primes) < primes_to_find):
+    if all(x % prime_number for prime_number in list_of_primes):
+        list_of_primes.append(x)
+    x += 1
+
+print('''
+TASK: What is the 10 001st prime number?
+      ''')
+#print(list_of_primes)
+print(list_of_primes[-1]) #last item (10001st)
