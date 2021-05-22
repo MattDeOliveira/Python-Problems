1c14dc92fc4253c071da52fa9c7244792fcb9cd9
MattDeOliveira <mattoli829@gmail.com>
2021-05-22 17:43:30 -0400

    Update problem_5.py
    completion of probelm 5... finally

--- problem_5.py
+++ problem_5.py
@@ -1,30 +1,23 @@
-#numbers = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
+target = 10
+x = 1
+switch = False #answer found
 
-def Find_multiples(n, x):
-    for i in range(1, x):
-        if x % i != 0:
-            return False   # NOT multiple
-        return True # multiple of every number
+while True:
+    for i in range(1, target+1): #1 - 20
+        if x % i != 0: # not evenly divisible
+            break
+        if i == target:
+            print(i)
+            switch = True
+    if switch:
+        break
+    x += 1
+print(x)
 
-def factorial(x):
-    if x > 1:
-        x * factorial(x - 1)
-        return x
-    elif x >= 0:
-        return 1
-    else:
-        return -1 #does not exist
-
-def smallest_positive_number(x):
-    for i in range(x, factorial(x) + 1, x):
-        if Find_multiples(i, x):
-            return i
-        return -1
-
-print(smallest_positive_number(20))
 
 
 print("""
 TASK: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """)
+print(x)
