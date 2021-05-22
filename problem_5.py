0638adeaf61b0243e400da2a9a05549af7f99992
MattDeOliveira <mattoli829@gmail.com>
2021-05-22 01:18:47 -0700

    added functions to find divisble number and multiple... tweaks still needed to be made

--- problem_5.py
+++ problem_5.py
@@ -1,20 +1,30 @@
-max = 20
-numbers = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
-n = 2520 # Ans divisible from 1-10
-switch = 0
-#while switch == 0:
-while not multiples(n, numbers):
-    n += 2520
+#numbers = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
 
+def Find_multiples(n, x):
+    for i in range(1, x):
+        if x % i != 0:
+            return False   # NOT multiple
+        return True # multiple of every number
 
-def multiples(n, numbs) : bool
-for x in numbs:
-    if (n % x) != 0:
-        return False    
+def factorial(x):
+    if x > 1:
+        x * factorial(x - 1)
+        return x
+    elif x >= 0:
+        return 1
+    else:
+        return -1 #does not exist
+
+def smallest_positive_number(x):
+    for i in range(x, factorial(x) + 1, x):
+        if Find_multiples(i, x):
+            return i
+        return -1
+
+print(smallest_positive_number(20))
 
 
 print("""
 TASK: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """)
-print(n)
