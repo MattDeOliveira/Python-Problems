ed6bdffe2d48a2e8b6d38cce5fc3e34f3a1cf2a8
MattDeOliveira <mattoli829@gmail.com>
2021-05-24 09:20:22 -0400

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -1,18 +1,17 @@
-target = 10
+target = 20
 x = 1
 switch = False #answer found
 
 while True:
     for i in range(1, target+1): #1 - 20
-        if x % i != 0: # not evenly divisible
+        if x % i != 0: # number found is not evenly divisible
             break
-        if i == target:
-            print(i)
+        if i == target: # target divisible by 'i'
             switch = True
-    if switch:
+    if switch: # breaks once answer is found
         break
-    x += 1
-print(x)
+    x += 1 # adds 1 until answer is found
+#print(x)
 
 
 
